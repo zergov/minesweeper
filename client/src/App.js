c7d15716c5524de4c868a6601ebefe2ae8528852
Jonathan Lalande <jonathan.lalande.1@ens.etsmtl.ca>
2019-05-24 10:42:37 -0400

    we are able to click on cells, individually :)

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from 'react'
 import './App.css'
 import axios from 'axios'
 
-const cell = () => ({ hidden: false, flag: false, value: 0})
-const createGrid = (width, height) => new Array(width * height).fill(cell())
+const cell = () => ({ hidden: true, flag: false, value: 0})
+const createGrid = (width, height) => new Array(width * height).fill(0).map(() => cell())
 
 function App() {
   const width = 10;
@@ -13,13 +13,22 @@ function App() {
   const onClick = index => {
     const x = index % width
     const y = Math.floor(index / width)
+
+    // update the grid
+    grid[index].hidden = false
+    setGrid([...grid])
+
     console.log(`clicked on cell (${x}, ${y})`, grid[index])
   }
 
   return (
     <div className="App">
       <main className="wrapper grid">
-        { grid.map((cell, i) => <div onClick={() => onClick(i)} key={i}>{cell.value}</div>) }
+        { grid.map((cell, i) => {
+          return (
+            <div onClick={() => onClick(i)} key={i}>{cell.hidden ? "" : cell.value}</div>
+          )
+        }) }
       </main>
     </div>
   )
