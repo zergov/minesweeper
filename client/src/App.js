faaa31e8a073ad4990bb5db50859baec6d9ee26a
Jonathan Lalande <jonathan.lalande.1@ens.etsmtl.ca>
2019-05-24 11:18:25 -0400

    move game logic inside minesweeper.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,18 @@
 import React, { useState, useEffect } from 'react'
 import './App.css'
+import { createGame, sweep } from './minesweeper'
 import Grid from './components/Grid'
 import axios from 'axios'
 
-const cell = () => ({ hidden: true, flag: false, value: 0})
-const createGrid = (width, height) => new Array(width * height).fill(0).map(() => cell())
 
 function Game() {
-  const width = 10;
-  const height = 10;
-  const [grid, setGrid] = useState(createGrid(width, height))
-
-  const onSweep = index => {
-    const x = index % width
-    const y = Math.floor(index / width)
-
-    // update the grid
-    grid[index].hidden = false
-    setGrid([...grid])
-
-    console.log(`clicked on cell (${x}, ${y})`, grid[index])
-  }
+  const [game, setGame] = useState(createGame(10, 10))
+  const onSweep = index => setGame(sweep(game, index))
 
   return (
     <div className="App">
       <h1>make a stats component here :)</h1>
-      <Grid onSweep={onSweep} grid={grid} />
+      <Grid onSweep={onSweep} grid={game.grid} />
     </div>
   )
 }
