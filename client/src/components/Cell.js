568183aaf53cfb0ab2f064015e8c22d94b3b9125
mdesson <michael.desson@gmail.com>
2019-05-24 11:58:54 -0400

    Cell updates with dangerLevel

--- client/src/components/Cell.js
+++ client/src/components/Cell.js
@@ -1,6 +1,15 @@
 import React from 'react'
 
 const Cell = ({onSweep, dangerLevel, flagged, hidden}) =>
-    <div className="cell" onClick={onSweep} flag={flagged}>{(hidden) ? "" : dangerLevel}</div>
+    <div className={(!hidden) ? "cell swept" : "cell"} onClick={onSweep} flag={flagged}>{(hidden) ? "" : cellValue(dangerLevel)}</div>
+
+
+const cellValue = (dangerLevel) => {
+    switch (dangerLevel) {
+        case -1: return "M"
+        case 0: return ""
+        default: return dangerLevel
+    }
+}
 
 export default Cell
