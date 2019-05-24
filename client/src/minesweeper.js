322375e67d13ca5e8455412494cd7ea0d8a25846
Jonathan Lalande <jonathan.lalande.1@ens.etsmtl.ca>
2019-05-24 14:38:12 -0400

    add expand feature

--- client/src/minesweeper.js
+++ client/src/minesweeper.js
@@ -11,14 +11,14 @@ export const createGame = (width, height) => {
     width,
     height,
     initialized: false,
-    mineCount: 40,
+    mineCount: 10,
     grid: createGrid(width, height),
   })
 }
 
-const neighbours = (game, index) => {
-  const x = Math.floor(index % game.width)
-  const y = Math.floor(index / game.width)
+const neighbours = (width, height, index) => {
+  const x = Math.floor(index % width)
+  const y = Math.floor(index / width)
   const surounding = [
     {x: x+1, y}, {x: x-1, y},
     {x, y: y+1}, {x, y: y-1},
@@ -26,31 +26,44 @@ const neighbours = (game, index) => {
     {x: x-1, y: y+1}, {x: x+1, y: y-1},
   ]
   return surounding
-    .filter(({x, y}) => (x >= 0 && x < game.width) && (y >= 0 && y < game.height))
-    .map(({x, y}) => x + game.width * y)
+    .filter(({x, y}) => (x >= 0 && x < width) && (y >= 0 && y < height))
+    .map(({x, y}) => x + width * y)
 }
 
 const initialize = game => {
   const mines = new Array(game.mineCount)
     .fill(game)
     .map(() => Math.floor(Math.random() * (game.width * game.height)))
-
   const grid = game.grid
     .map((cell, i) => mines.includes(i) ? { ...cell, value: -1} : cell) // assign mines
     .map((cell, i) => {
       if (cell.value == -1) return cell
 
-      const value = neighbours(game, i).filter(neighbour => mines.includes(neighbour)).length
+      const value = neighbours(game.width, game.height, i).filter(neighbour => mines.includes(neighbour)).length
       return {...cell, value}
     })
 
   return {...game, initialized: true, grid}
 }
 
+const expand = (grid, width, height, index) => {
+  const cell = grid[index]
+  grid[index] = {...cell, hidden: false}
+
+  if (cell.value !== 0) return grid // if its a bomb or a value
+  if (!cell.hidden) return grid // if its a bomb or a value
+
+  grid[index] = {...cell, hidden: false}
+
+  const surounding = neighbours(width, height, index)
+  return surounding.reduce((grid, neighbour) => expand(grid, width, height, neighbour), grid)
+}
+
 export const sweep = (game, index) => {
-  // if the game is not initialized, generate and add mines to the grid
   if (!game.initialized) game = initialize(game)
 
-  const grid = game.grid.map((cell, i) => i == index ? {...cell, hidden: false} : cell)
-  return {...game, grid}
+  return {
+    ...game,
+    grid: expand([...game.grid], game.width, game.height, index)
+  }
 }
